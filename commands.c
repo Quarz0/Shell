ac9b51c82294bea3df57baf5287a83de9e2f820f
Marwan Tammam <marwan.01010100@gmail.com>
2017-10-07 11:29:28 +0200

    Moved some special commands from main

--- commands.c
+++ commands.c
@@ -1,13 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "variables.h"
 #include "commands.h"
 
+void cd(const char *path) {
+    if (path == NULL || strcmp(path, "~") == 0) {
+        char *home = lookup_variable("HOME");
+        chdir(home);
+        free(home);
+    }
+}
 
-void cd( const char* path )
-{
-	// you should implement this function
-}  
+void assign_variable(const char *assignment) {
+    if (!isalpha(assignment[0])) {
+        error(assignment);
+        return;
+    }
+    int index = (int) (strchr(assignment, '=') - assignment);
+    char key[index + 1];
+    char val[strlen(assignment) - index + 1];
+    memcpy(key, assignment, index);
+    memcpy(val, assignment + index + 1, strlen(assignment) - index);
+    key[index] = '\0';
+    val[strlen(assignment) - index] = '\0';
+    set_variable(key, val);
+}
 
-
-void echo( const char* message )
-{
-	// you should implement this function
+void error(const char *command) {
+    printf("shell: %s: command not found\n", command);
 }
--- commands.h
+++ commands.h
@@ -4,12 +4,10 @@
 /* 
 	- This function should be responsible for implementing the "cd" shell command
 */
-void cd( const char* path );  
+void cd(const char *path);
 
-/* 
-	- This function should be responsible for implementing the "echo" shell command
-	- Typically should lookup any variable included in the message
-*/
-void echo( const char* message );  
+void assign_variable(const char *assignment);
+
+void error(const char *command);
 
 #endif // COMMANDS_H_
